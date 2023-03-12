1e412c5d57e974b7a71607a129a5f4b3e433fa3f
iantelli <2ndex@duck.com>
2023-03-11 16:10:44 -0800

    Chat bubble css

---
+++ src/components/ChatBubble.tsx
@@ -0,0 +1,11 @@
+type ChatBubbleProps = {
+    inverted?: boolean
+    text: string
+}
+
+export default function ChatBubble({ inverted, text }: ChatBubbleProps) {
+    return (
+        <>
+        </>
+    )
+}
