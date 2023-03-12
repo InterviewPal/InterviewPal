e889055836e0a7384621a9bb87cfb4f6a5772404
Kiefer Hogg <kieferhogg@gmail.com>
2023-03-11 18:17:32 -0800

    added interview page

--- src/pages/interview/index.tsx
+++ src/pages/interview/index.tsx
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
 import Navbar from "@/components/Navbar";
+import Image from 'next/image';
 
 export default function Home() {
     const [theme, setTheme] = useState(null as 'light' | 'dark' | null)
+    const [question, setQuestion] = useState(null as string | null)
 
     useEffect(() => {
         const theme = localStorage.getItem('theme');
@@ -37,6 +39,11 @@ export default function Home() {
         }
     };
 
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(e.target.answer.value);
+    }
+
     return (
         <>
             <Head>
@@ -46,11 +53,24 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Navbar logo={false} theme={theme} onClick={handleThemeChange} />
-            <main>
-                {/* 2 wide flex with one element beign 1/5 wide the other being 4/5 wide */}
+            <main className="text-rosePineDawn-text dark:text-rosePineMoon-text">
                 <div className="flex flex-row">
-                    <div className="flex flex-col w-1/5 bg-white"></div>
-                    <div className="flex flex-col w-4/5 bg-red-900"></div>
+                    <div className="flex flex-col w-1/5 items-center">
+                        <Image src="/logo.png" alt="Icon" width={500} height={500} className="w-1/2 -mb-14 z-10" />
+                        <Image src="/suit.svg" alt="Suit" width={500} height={500} />
+                    </div>
+                    <div className="flex flex-col w-4/5 bg-rosePineDawn-highlightMed dark:bg-rosePine-highlightMed justify-end">
+                        {question === null ? (
+                            <div className="flex flex-col items-center py-24 xl:py-44">
+                            <h1 className="text-4xl font-bold">Question</h1>
+                            <p className="text-xl mt-4">{question}</p>
+                        </div>
+                        ) : undefined}
+                        <form method="post" onSubmit={handleSubmit} className="flex flex-row">
+                            <textarea name="answer" id="answer" className="bg-rosePineDawn-highlightLow dark:bg-rosePine-highlightLow resize-none w-full dark:border-white border-black border-2 p-4" placeholder="Type your answer here..."></textarea>
+                            <button type="submit" className="bg-rosePineDawn-highlightLow dark:bg-rosePine-highlightLow p-4 dark:border-white border-black border-2">Submit</button>
+                        </form>
+                    </div>
                 </div>
             </main>
         </>
