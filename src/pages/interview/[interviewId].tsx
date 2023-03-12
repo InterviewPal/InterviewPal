18ab6a6d9f0fc24f5493f8cea76836bbf264bf38
Kiefer Hogg <kieferhogg@gmail.com>
2023-03-11 22:19:17 -0800

    ADDED STYLE

--- src/pages/interview/[interviewId].tsx
+++ src/pages/interview/[interviewId].tsx
@@ -29,12 +29,12 @@ export default function Home() {
         console.log();
     }
 
-    if (notFound) {
-        return <div>Interview Not found</div>;
-    }
-    if (interviewId === undefined || !isAuthed || !isDone) {
-        return <Loading />;
-    }
+    // if (notFound) {
+    //     return <div>Interview Not found</div>;
+    // }
+    // if (interviewId === undefined || !isAuthed || !isDone) {
+    //     return <Loading />;
+    // }
 
     return (
         <>
@@ -45,31 +45,31 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Navbar logo={false} />
-            <main className="text-rosePineDawn-text dark:text-rosePineMoon-text overflow-hidden w-full h-full relative">
+            <main className="text-rosePineDawn-text dark:text-rosePine-text overflow-hidden w-full h-full relative">
                 {/* Middle */}
                 <div className="flex h-full flex-1 flex-col md:pl-[280px]">
                     <div className="relative h-full w-full transition-width flex flex-col overflow-hidden items-stretch flex-1">
                         {/* Text Area */}
                         {/* Imput Area */}
                         <div className="absolute bottom-0 left-0 w-full">
-                            <form onSubmit={handleSubmit} className="flex flex-col">
-                                <textarea
-                                    name="answer"
-                                    className="w-full h-24 p-4 bg-rosePineDawn-bg dark:bg-rosePineMoon-bg"
-                                    placeholder="Type your answer here..."
-                                />
-                                <button
-                                    type="submit"
-                                    className="w-full h-12 bg-rosePineDawn-bg dark:bg-rosePineMoon-bg rounded-b-lg"
-                                >
-                                    Submit
-                                </button>
+                            <form onSubmit={handleSubmit} className="stretch mx-2 flex flex-row gap-3 last:mb-2 md:last:mb-6 lg:mx-auto lg:max-w-3xl">
+                                <div className="flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative border-2 border-rosePineDawn-highlightHigh dark:border-rosePine-highlightHigh rounded-md bg-rosePineDawn-highlightLow dark:bg-rosePine-highlightLow">
+                                    <textarea className="message-text-area m-0 w-full resize-none border-0 focus:ring-0 bg-transparent p-0 pl-2 pr-7 md:pl-0" rows={1} placeholder="Type your answer here" tabIndex={1} />
+                                    <button
+                                        type="submit"
+                                        className="absolute p-1 rounded-md text-gray-500 bottom-1.5 right-1 md:bottom-2.5 md:right-2 bg-rosePine-foam hover:bg-rosePineDawn-foam dark:bg-rosePineDawn-love hover:dark:bg-rosePine-love"
+                                    >
+                                        <svg
+                                            className="h-4 w-4 mr-1"
+                                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480V396.4c0-4 1.5-7.8 4.2-10.7L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z" /></svg>
+                                    </button>
+                                </div>
                             </form>
                         </div>
                     </div>
                 </div>
                 {/* Left Side */}
-                <div className="hidden bg-gray-500 md:fixed md:top-24 md:bottom-0 md:flex md:w-[280px] md:flex-col">
+                <div className="hidden md:fixed md:top-24 md:bottom-0 md:flex md:w-[280px] md:flex-col -mt-8 ml-4">
                     <Image src="/suit-logo.png" alt="hero" width={280} height={280} />
                 </div>
             </main>
